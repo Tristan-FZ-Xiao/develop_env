290b47c7d84d81af8ad003df029fb92caeeb267c
Tristan <tristan_xiao@qq.com>
2017-10-25 21:13:42 +0800

    feat(Call_Analysis): add filter feature

--- call_analysis.sh
+++ call_analysis.sh
@@ -5,10 +5,13 @@
 # 2. tceetree
 # 3. dot
 
-call_analysis() { cscope -Rcbkq -P $1; tceetree -f -r $2 -o /proc/self/fd/1 | dot -Tpdf -o $3; }
+echo "call_analysis {path} {target_call} {filter} {output}"
+
+call_analysis() { cscope -Rcbkq -P $1; tceetree -f -r $2 -o /proc/self/fd/1 | egrep -v "$3" | dot -Tpdf -o $4; }
 
 path="$1"
 target="$2"
-output="$3"
+filter="$3"
+output="$4"
 
-call_analysis "$path" "$target" "$output"
+call_analysis "$path" "$target" "filter" "$output"
